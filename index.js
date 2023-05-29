5920b9beaffa4c83d0c4e547e752ae3761bd4829
Nikhil Bhawsar <nbhawsar@adobe.com>
2023-05-29 15:52:14 +0530

    adding placeholder

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("you are awesome");
+const fs = require('fs'),
+http = require('http');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
